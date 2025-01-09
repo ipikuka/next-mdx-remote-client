a44b9096ab078bc37d54c59b957eccd5193ce7cc
talatkuyuk <talatkuyuk@gmail.com>
2025-01-10 00:28:55 +0300

    updated the eslint config inline with eslint 9

--- tests/ErrorBoundary.jsx
+++ tests/ErrorBoundary.jsx
@@ -7,15 +7,18 @@ class ErrorBoundary extends React.Component {
     // Define a state variable to track whether is an error or not
     this.state = { hasError: false };
   }
+
+  // eslint-disable-next-line no-unused-vars
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI
 
     return { hasError: true };
   }
+
   componentDidCatch(error, errorInfo) {
-    // You can use your own error logging service here
     console.log({ error, errorInfo });
   }
+
   render() {
     // Check if the error is thrown
     if (this.state.hasError) {
